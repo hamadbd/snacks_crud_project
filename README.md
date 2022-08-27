0b5dffa73fbf633427ed2a42e452ca4fe37ba7b6
Hamad Dwairi <hamaddwairi0@gmail.com>
2022-08-28 00:40:46 +0300

    feature tasks complete

---
+++ README.md
@@ -0,0 +1,4 @@
+# CRUD w/ DJANGO
+
+> this repo is an my example of how to set up CRUD actions using the DJANGO framework
+
---
+++ snacks/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.views.generic import ListView,DetailView,CreateView,DeleteView,UpdateView
+from .models import Snack
+from django.urls import reverse_lazy
+
+# Create your views here.
+
+class SnackListView(ListView):
+    template_name = 'snack_list.html'
+    model = Snack
+    
+
+class SnackDetailView(DetailView):
+    template_name = 'snack_detail.html'
+    model = Snack
+    
+
+class SnackCreateView(CreateView):
+    template_name = 'snack_create.html'
+    model = Snack
+    fields = ['title','purchaser','description']
+    success_url = reverse_lazy('snack_list')
+
+class SnackDeleteView(DeleteView):
+    template_name = 'snack_delete.html'
+    model = Snack
+    success_url = reverse_lazy('snack_list')
+
+class SnackUpdateView(UpdateView):
+    template_name = 'snack_update.html'
+    model = Snack
+    fields = ['title','description','purchaser']
+    success_url = reverse_lazy('snack_list')
